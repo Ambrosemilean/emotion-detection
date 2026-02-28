3e59671364b652ce8539a2b0faf845664bac9280
Uwakwe Ambrose Ikenna <ikennaambrose60@gmail.com>
2026-03-01 00:16:06 +0100

    updated repository

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,4 +3,14 @@ import json
 def emotion_detector(text_to_analyze):
     """Function to detect the emotions in text using Watson NLP"""
     if not text_to_analyze:
-        return
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        url = 'https://sn-watson-emotion.Lab.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+        headers ={"grpc-metadata-mm-model-id"}
+        if response.status_code == 200
